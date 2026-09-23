894f0df6dd7b405d3130757f458b91b9946563df
agungtp22 <agungtp22@users.noreply.github.com>
2026-09-23 10:39:36 +0700

    fix(cask): correct SHA256 arm and add missing https:// prefix in URL

--- Casks/pranata.rb
+++ Casks/pranata.rb
@@ -2,10 +2,10 @@ cask "pranata" do
   arch arm: "aarch64", intel: "x64"
 
   version "2.0.0"
-  sha256 arm:   "fc35e2bbe99a12528bad4232a5431add91cf44a7f5ec3dfd27dc6a71d4b11afc",
+  sha256 arm:   "2eec55b7068f8357387049b3c1be450317014eba46d3f8cd0c50fb667e906168",
          intel: "ca23d970936964c0f3ee3a32dfaf2cbc8cb16dbf1a205ff069413438d491485f"
 
-  url "is3.cloudhost.id/pranata/Pranata_#{version}_#{arch}.dmg"
+  url "https://is3.cloudhost.id/pranata/Pranata_#{version}_#{arch}.dmg"
   name "Pranata"
   desc "Modern, lightweight, offline-first API client with Postman parity"
   homepage "https://github.com/agungtp22/pranata"
