481c4e1a88ba02a0bf9325b2822f733c3f0f4591
Agung Tri Pambudi <agung.2204@gmail.com>
2026-09-23 09:29:40 +0700

    Fix URL for Pranata download location

--- Casks/pranata.rb
+++ Casks/pranata.rb
@@ -5,7 +5,7 @@ cask "pranata" do
   sha256 arm:   "fc35e2bbe99a12528bad4232a5431add91cf44a7f5ec3dfd27dc6a71d4b11afc",
          intel: "ca23d970936964c0f3ee3a32dfaf2cbc8cb16dbf1a205ff069413438d491485f"
 
-  url "is3.cloudhost.id/Pranata_#{version}_#{arch}.dmg"
+  url "is3.cloudhost.id/pranata/Pranata_#{version}_#{arch}.dmg"
   name "Pranata"
   desc "Modern, lightweight, offline-first API client with Postman parity"
   homepage "https://github.com/agungtp22/pranata"
